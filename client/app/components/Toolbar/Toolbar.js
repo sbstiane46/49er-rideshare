32ea28cfe861f13225bc3bd37face840069d1ca7
Anitra <anitra.oursky@gmail.com>
2019-09-14 13:06:22 -0400

    Add toggle button to toolbar

--- client/app/components/Toolbar/Toolbar.js
+++ client/app/components/Toolbar/Toolbar.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
 import './Toolbar.scss';
 
 
 const Toolbar = props => (
     <header className="toolbar">
         <nav className="toolbar_navigation">
-            <div></div>
+            <div>
+                <DrawerToggleButton />
+            </div>
             <div className="toolbar_logo"><a href="/">49er Rideshare Logo</a></div>
+            <div className="spacer" />
             <div className="toolbar_navigation-items">
                 <ul>
                     <li><a href="#">User Profile</a></li>
