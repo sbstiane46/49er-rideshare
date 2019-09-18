d24e8f4e8db78b9a4978b462b03e46658c0db8c8
Anitra <anitra.oursky@gmail.com>
2019-09-17 20:17:58 -0400

    Uglifyjs-3

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,32 +1,6 @@
-// const webpack = require('webpack');
-// const merge = require('webpack-merge');
-// const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-
-// const helpers = require('./helpers');
-// const commonConfig = require('./webpack.common');
-
-// module.exports = merge(commonConfig, {
-//   mode: 'production',
-
-//   output: {
-//     filename: 'js/[name].[hash].js',
-//     chunkFilename: '[id].[hash].chunk.js'
-//   },
-
-//   plugins: [
-//     new UglifyJsPlugin({
-//       uglifyOptions: {
-//         warnings: false,
-//         ie8: false,
-//         output: {
-//           comments: false
-//         }
-//       }
-//     })
-//   ]
-// });
 const webpack = require('webpack');
 const merge = require('webpack-merge');
+const UglifyJsPlugin = require('uglifyjs-3-webpack-plugin');
 
 const helpers = require('./helpers');
 const commonConfig = require('./webpack.common');
@@ -40,14 +14,14 @@ module.exports = merge(commonConfig, {
   },
 
   plugins: [
-    new config.optimization.minimize({
-      compressor: {
-        warnings: false,
-        screw_ie8: true
-      },
-      output: {
-        comments: false
-      }
+    new UglifyJsPlugin({
+    uglifyOptions: {
+    warnings: false,
+    ie8: false,
+    output: {
+    comments: false
+    }
+    }
     })
-  ]
+    ]
 });
