a8b0a074f9cea9444c4e1ee0622eaffce25c2b81
Winston Kyle Davis <thiscatiskool@Winstons-MacBook-Pro.local>
2019-09-24 21:12:16 -0400

    updated the sidedrawer so that the user modal is only rendered when called for

--- client/app/components/SideDrawer/SideDrawer.js
+++ client/app/components/SideDrawer/SideDrawer.js
@@ -3,23 +3,19 @@ import { UserProfile } from './UserProfile';
 import './SideDrawer.scss';
 
 const sideDrawer = props => {
-    let drawerClasses = 'side-drawer';
-    if (props.show) {
-        drawerClasses = 'side-drawer open';
-    }
-    return (
-    <nav className={drawerClasses}>
-        <ul>
-            <li>
-                <a href="#">Where to?</a>
-            </li>
-            <li>
-                <a href="#">Find Driver</a>
-            </li>
-            <li><UserProfile /></li>
-        </ul>
-    </nav>
-    );
+    return props.show ? 
+        (<nav className='side-drawer open'>
+            <ul>
+                <li>
+                    <a href="#">Where to?</a>
+                </li>
+                <li>
+                    <a href="#">Find Driver</a>
+                </li>
+                <li><UserProfile /></li>
+            </ul>
+        </nav>)
+        : <></>
 };
 
-export default sideDrawer;
+export default sideDrawer
